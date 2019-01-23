5b5693c3643cf70f710c20bbf334002a745ec1d7
AlejandroHdeez <jaandrohernandez@gmail.com>
2019-01-22 22:06:36 -0600

    Coreccion de Complementos y implementacion de navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+{path: 'navbar', component: NavbarComponent},
+{path: 'home', component: HomePageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
